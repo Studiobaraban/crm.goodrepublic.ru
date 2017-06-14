8b04903cd94d672f535087f5ccad91a10ec24314
studiobaraban <studiobaraban@gmail.com>
2017-06-14 23:08:20 +0300

    Редактирование Абонементов

--- public_html/views/abonements/_form.php
+++ public_html/views/abonements/_form.php
@@ -4,7 +4,6 @@ use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
 use nex\datepicker\DatePicker;
-use app\models\Persons;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Abonements */
@@ -16,54 +15,57 @@ use app\models\Persons;
     <?php $form = ActiveForm::begin();
 
 
-    echo $form->field($model, 'user_id')->textInput();
+    echo $form->field($model, 'user_id')->textInput(); ?>
 
-
-    echo DatePicker::widget([
-        'model' => $model,
-        'attribute' => 'start',
-        'language' => 'ru',
-        'size' => 'sm',
-        'readonly' => false,
-        'placeholder' => 'Выберите дату',
-        'clientOptions' => [
-        'format' => 'YYYY-MM-DD LT',
+    <div class="row">
+        <div class="col-xs-12 col-md-6">
+            <label class="control-label">Дата начала</label>
+            <?= DatePicker::widget([
+                'model' => $model,
+                'attribute' => 'start',
+                'language' => 'ru',
+                'readonly' => false,
+                'placeholder' => 'Выберите дату',
+                'class' => 'form-control',
+                'clientOptions' => [
+                'format' => 'YYYY-MM-DD LT',
                 //'minDate' => '2015-08-10',
                 //'maxDate' => '2015-09-10',
-        ],
-        ]); 
-
-    echo DatePicker::widget([
-        'model' => $model,
-        'attribute' => 'end',
-        'language' => 'ru',
-        'size' => 'sm',
-        'readonly' => false,
-        'placeholder' => 'Выберите дату',
-        'clientOptions' => [
-        'format' => 'YYYY-MM-DD LT',
+                ],
+                ]);?> 
+            </div>
+            <div class="col-xs-12 col-md-6">
+                <label class="control-label">Окончание</label>
+                <?= DatePicker::widget([
+                    'model' => $model,
+                    'attribute' => 'end',
+                    'language' => 'ru',
+                    'readonly' => false,
+                    'placeholder' => 'Выберите дату',
+                    'clientOptions' => [
+                    'format' => 'YYYY-MM-DD LT',
                 //'minDate' => '2015-08-10',
                 //'maxDate' => '2015-09-10',
-        ],
-    ]);
-    ?>
+                    ],
+                    ]);?>
+                </div>
+            </div>
+            <?= $form->field($model, 'countvis')->textInput() ?>
 
-    <?= $form->field($model, 'countvis')->textInput() ?>
+            <?= $form->field($model, 'balance')->textInput() ?>
 
-    <?= $form->field($model, 'balance')->textInput() ?>
+            <?= $form->field($model, 'price')->textInput() ?>
 
-    <?= $form->field($model, 'price')->textInput() ?>
+            <?= $form->field($model, 'type')->textInput() ?>
 
-    <?= $form->field($model, 'type')->textInput() ?>
- 
-    <?= $form->field($model, 'status')->textInput() ?>
+            <?= $form->field($model, 'status')->textInput() ?>
 
-    <?= $form->field($model, 'info')->textarea(['rows' => 6]) ?>
+            <?= $form->field($model, 'info')->textarea(['rows' => 6]) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
+            <div class="form-group">
+                <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+            </div>
 
-    <?php ActiveForm::end(); ?>
+            <?php ActiveForm::end(); ?>
 
-</div>
+        </div>
--- public_html/views/abonements/create.php
+++ public_html/views/abonements/create.php
@@ -16,61 +16,8 @@ $this->title = 'Создать абонемент';
 
 	<h1><?= Html::encode($this->title) ?></h1>
 
-	<div class="abonements-form">
+	<?= $this->render('_form', [
+		'model' => $model,
+		]) ?>
 
-		<?php $form = ActiveForm::begin();
-
-
-		echo $form->field($model, 'user_id')->textInput();
-
-
-		echo DatePicker::widget([
-			'model' => $model,
-			'attribute' => 'start',
-			'language' => 'ru',
-			'size' => 'sm',
-			'readonly' => false,
-			'placeholder' => 'Выберите дату',
-			'clientOptions' => [
-			'format' => 'YYYY-MM-DD LT',
-                //'minDate' => '2015-08-10',
-                //'maxDate' => '2015-09-10',
-			],
-			]); 
-
-		echo DatePicker::widget([
-			'model' => $model,
-			'attribute' => 'end',
-			'language' => 'ru',
-			'size' => 'sm',
-			'readonly' => false,
-			'placeholder' => 'Выберите дату',
-			'clientOptions' => [
-			'format' => 'YYYY-MM-DD LT',
-                //'minDate' => '2015-08-10',
-                //'maxDate' => '2015-09-10',
-			],
-			]);
-			?>
-
-			<?= $form->field($model, 'countvis')->textInput() ?>
-
-			<?= $form->field($model, 'balance')->textInput() ?>
-
-			<?= $form->field($model, 'price')->textInput() ?>
-
-			<?= $form->field($model, 'type')->textInput() ?>
-			
-			<?= $form->field($model, 'status')->textInput(['value' => 1]) ?>
-
-			<?= $form->field($model, 'info')->textarea(['rows' => 6]) ?>
-
-			<div class="form-group">
-				<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-			</div>
-
-			<?php ActiveForm::end(); ?>
-
-		</div>
-
-	</div>
+</div>
